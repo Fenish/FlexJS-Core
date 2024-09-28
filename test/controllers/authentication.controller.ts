0d9e7f9c5d4ac11d89c597616b4aef73cf0d8b42
Fenish <sohretalhadev@outlook.com>
2024-09-28 18:11:26 +0300

    Fix typo

--- test/controllers/authentication.controller.ts
+++ test/controllers/authentication.controller.ts
@@ -2,19 +2,18 @@ import { Controller, ResponseStatus, Get } from '../../src/decorators';
 
 @Controller('/auth')
 export class AuthenticationController {
-    
-    @Get('/login')
-    login() {
-        return {
-            message: 'login'
-        };
-    }
+	@Get('/login')
+	login() {
+		return {
+			message: 'login',
+		};
+	}
 
-    @Get('/register')
-    @ResponseStatus(201)
-    register() {
-        return {
-            message: 'register'
-        };
-    }
+	@Get('/register')
+	@ResponseStatus(201)
+	register() {
+		return {
+			message: 'register',
+		};
+	}
 }
