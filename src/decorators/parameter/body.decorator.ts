2168db69383010d603eb812ef0583a9614bf653d
Fenish <sohretalhadev@outlook.com>
2024-09-29 17:01:13 +0300

    Add body parameter decorator

--- src/decorators/parameter/body.decorator.ts
+++ src/decorators/parameter/body.decorator.ts
@@ -1,6 +1,20 @@
-export function Body() {
-	return function (target: any, propertyKey: string, context: any) {
-		console.log(target, propertyKey, context);
-		Reflect.defineMetadata('body', true, target, context.name);
+import { BODY_METADATA_KEY } from '@/metadata-keys';
+
+export function Body(key?: string) {
+	return function (
+		target: any,
+		propertyKey: string | symbol,
+		parameterIndex: number
+	) {
+		const existingBodyParams: { index: number; key?: string }[] =
+			Reflect.getMetadata(BODY_METADATA_KEY, target, propertyKey) || [];
+		existingBodyParams.push({ index: parameterIndex, key });
+
+		Reflect.defineMetadata(
+			BODY_METADATA_KEY,
+			existingBodyParams,
+			target,
+			propertyKey
+		);
 	};
 }
