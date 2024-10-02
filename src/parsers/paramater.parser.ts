aa45be321799f2db93144fe34b1950f3884ba7b4
Fenish <sohretalhadev@outlook.com>
2024-10-03 01:47:02 +0300

    Make parser dynamic

--- src/parsers/paramater.parser.ts
+++ src/parsers/paramater.parser.ts
@@ -1,31 +1,28 @@
-import {
-	BODY_METADATA_KEY,
-	FUNCTION_PARAMETERS_METADATA_KEY,
-} from '../metadata-keys';
-import { FlexRequest } from '../types';
+import { FUNCTION_PARAMETERS_METADATA_KEY } from '../metadata-keys';
+import { FlexRequest, FlexResponse } from '../types';
 import { Logger } from '../utils/logger';
 
-export async function parseParameters(req: FlexRequest, route: any) {
+export async function parseParameters(
+	req: FlexRequest,
+	res: FlexResponse,
+	route: any
+) {
 	Logger.debug('Parsing Parameters');
 	const parameters = route[FUNCTION_PARAMETERS_METADATA_KEY];
+
+	// sort based on param.data.index
+	parameters.sort((a: any, b: any) => {
+		return a.data.index - b.data.index;
+	});
 
 	const orderedParams: any = [];
 	for (const param of parameters) {
 		Logger.debug('Parsing parameter: ' + JSON.stringify(param.data));
-		if (param.type === BODY_METADATA_KEY) {
-			const result = parseBody(req, param.data);
-			orderedParams.push(result);
+		if (param.data.callback) {
+			const data = param.data.callback(req, res);
+			orderedParams.push(data);
 		}
 	}
 
 	route.parameters = orderedParams;
-}
-
-function parseBody(req: FlexRequest, data: any) {
-	const body = req.body;
-	if (data.key) {
-		return body[data.key];
-	} else {
-		return body;
-	}
 }
